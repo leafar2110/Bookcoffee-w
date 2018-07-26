fbdb6b85f5d84016bcf1a008f8beab79a54fc71d
Yonnys Quintero <leafar.qr@gmail.com>
2018-07-26 12:35:02 -0400

    Descripcion del menu

--- wp-content/themes/theme-bookcoffee/parciales/populardishes.php
+++ wp-content/themes/theme-bookcoffee/parciales/populardishes.php
@@ -2,90 +2,96 @@
 	<div class="container">
 
 		<div class="titulo-menu">
-		<h1>Popular Dishes</h1>
-		<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident.</p>
+			<h1>Popular Dishes</h1>
+			<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident.</p>
 		</div>
 		<div class="container">
 			<div class="row">
 				<div class="col-md-4">
+				  <a href=" <?php echo bloginfo('url').'/index.php/description'; ?>">
 					<div class="contenedor" >
 						<div class="article-app item e1">
 							<img src="<?php echo get_template_directory_uri();?>/assets/img/espresso.jpg" class="img-fluid img-app" >
 							<div class="separador text-center">
-									<h4>Lorem ipsum</h4>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,</p>
-									<h3>$ 9.99</h3>
-							</div>
-						</div>
-					</div>
-				</div>
-
-				<div class="col-md-4">
-					<div class="contenedor" >
-						<div class="article-app item e1">
-							<img src="<?php echo get_template_directory_uri();?>/assets/img/espresso.jpg" class="img-fluid img-app" >
-							<div class="separador text-center">
-									<h4>Lorem ipsum</h4>
+								<h4>Lorem ipsum</h4>
 								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,</p>
 								<h3>$ 9.99</h3>
-							</div>
+							</div>	
 						</div>
 					</div>
+				  </a>
 				</div>
 
 				<div class="col-md-4">
+				  <a href=" <?php echo bloginfo('url').'/index.php/description'; ?>">
 					<div class="contenedor" >
 						<div class="article-app item e1">
 							<img src="<?php echo get_template_directory_uri();?>/assets/img/espresso.jpg" class="img-fluid img-app" >
 							<div class="separador text-center">
 								<h4>Lorem ipsum</h4>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,</p>
-									<h3>$ 9.99</h3>
-							</div>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,</p>
+								<h3>$ 9.99</h3>
+							</div>	
 						</div>
 					</div>
+				  </a>
 				</div>
-
 				<div class="col-md-4">
+				  <a href=" <?php echo bloginfo('url').'/index.php/description'; ?>">
 					<div class="contenedor" >
 						<div class="article-app item e1">
 							<img src="<?php echo get_template_directory_uri();?>/assets/img/espresso.jpg" class="img-fluid img-app" >
 							<div class="separador text-center">
 								<h4>Lorem ipsum</h4>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,</p>
-									<h3>$ 9.99</h3>
-							</div>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,</p>
+								<h3>$ 9.99</h3>
+							</div>	
 						</div>
 					</div>
+				  </a>
 				</div>
-
 				<div class="col-md-4">
+				  <a href=" <?php echo bloginfo('url').'/index.php/description'; ?>">
 					<div class="contenedor" >
 						<div class="article-app item e1">
 							<img src="<?php echo get_template_directory_uri();?>/assets/img/espresso.jpg" class="img-fluid img-app" >
 							<div class="separador text-center">
-									<h4>Lorem ipsum</h4>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,</p>
-									<h3>$ 9.99</h3>
-							</div>
+								<h4>Lorem ipsum</h4>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,</p>
+								<h3>$ 9.99</h3>
+							</div>	
 						</div>
 					</div>
+				  </a>
 				</div>
-
 				<div class="col-md-4">
+				  <a href=" <?php echo bloginfo('url').'/index.php/description'; ?>">
 					<div class="contenedor" >
 						<div class="article-app item e1">
 							<img src="<?php echo get_template_directory_uri();?>/assets/img/espresso.jpg" class="img-fluid img-app" >
 							<div class="separador text-center">
-									<h4>Lorem ipsum</h4>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,</p>
-									<h3>$ 9.99</h3>
-							</div>
+								<h4>Lorem ipsum</h4>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,</p>
+								<h3>$ 9.99</h3>
+							</div>	
 						</div>
 					</div>
+				  </a>
 				</div>
-				
-
+				<div class="col-md-4">
+				  <a href=" <?php echo bloginfo('url').'/index.php/description'; ?>">
+					<div class="contenedor" >
+						<div class="article-app item e1">
+							<img src="<?php echo get_template_directory_uri();?>/assets/img/espresso.jpg" class="img-fluid img-app" >
+							<div class="separador text-center">
+								<h4>Lorem ipsum</h4>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,</p>
+								<h3>$ 9.99</h3>
+							</div>	
+						</div>
+					</div>
+				 </a>
+				</div>
 		</div>
 		</div>
 
