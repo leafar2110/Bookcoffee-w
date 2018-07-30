4a42cad8da4c1f7807ce0f4f41f035984626b926
Yonnys Quintero <leafar.qr@gmail.com>
2018-07-30 19:26:06 -0400

    responsive modal listo

--- wp-content/themes/theme-bookcoffee/header.php
+++ wp-content/themes/theme-bookcoffee/header.php
@@ -39,7 +39,7 @@
   <div class="modal-dialog " role="document">
     <div class="modal-content reserva reserva-modal">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel"><legend>Table reservation</legend></h5>
+        <legend>Table reservation</legend>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
