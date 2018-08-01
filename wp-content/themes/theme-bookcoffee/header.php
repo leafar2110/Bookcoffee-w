a9c7f7c5da01c8ff52b608e5e53b9b230c6b1ce8
VictorDiazE <diazegurbide@gmail.com>
2018-08-01 13:04:10 -0400

    acomodando el modal

--- wp-content/themes/theme-bookcoffee/header.php
+++ wp-content/themes/theme-bookcoffee/header.php
@@ -19,7 +19,7 @@
 			<nav class="navbar navbar-expand-lg navbar-light nav-app">
 				<div class="container">
 				  <a class="navbar-brand" href="<?php echo bloginfo('url').'/index.php'; ?>"><img src="<?php echo get_template_directory_uri();?>/assets/img/2.png" class="nav-logo img-fluid"></a>
-					<button type="button" class="navbar-toggler icom-tablet" data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap" aria-expanded="false">
+					<button type="button" class="navbar-toggler icom-tablet" data-toggle="modal" data-target=""" aria-expanded="false">
 						<i class="fa fa-cutlery icom" ></i></button>
 					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 						<i class="fa fa-bars icom"></i>
