44b52130a29e0ea62c543f2e859d5c85e870f368
VictorDiazE <diazegurbide@gmail.com>
2018-08-03 15:44:20 -0400

    acomodando el modal

--- wp-content/themes/theme-bookcoffee/page-home.php
+++ wp-content/themes/theme-bookcoffee/page-home.php
@@ -15,31 +15,14 @@
 				<div class="titulo-portada-1 wow fadeInUp col-md-6 col-sm-6">
 					<h1><span> Bookcoffee</span> Restaurant </h1>
 			  </div>
-			  <div class="col-sm-6 col-md-4">
-			  	<form class="reserva reserva-home wow fadeInRightBig">
-			  		<legend>Table reservation</legend>
-			  		<label>Personas</label>
-			  		<select type="text" class="form-control form-reserva personas" name="persona">
-			  			<option value="">Persons</option>
-							<option value="">1</option>
-							<option value="">2</option>
-							<option value="">3</option>
-							<option value="">4</option>
-							<option value="">5+</option>
-			  		</select> 
-			  		<label>Date</label>
-			  		<input type="date" class="form-control form-reserva" name="fecha">
-			  		<label>Time</label>
-			  		<input type="time" class="form-control form-reserva" name="hora">
-			  		<input type="submit" class="bottom-app btn-reserva" value="Reserve now" name="hora">
-			  	</form>
-			  </div>
+			  
+			  
 			</div>
 
 				
 			</div>
 		</header>
-
+		
 	</section>
 <!--  PLATOS-->
 <?php get_template_part('/parciales/populardishes')?>
